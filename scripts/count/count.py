8e74407266e06c0e2afb26e8dc4bb0623805cfb6
Zhiwei Li <lzwjava@gmail.com>
2025-08-02 05:02:08 +0800

    fix(count): correct count logic in count.py

--- scripts/count/count.py
+++ scripts/count/count.py
@@ -6,8 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 from count_py import count_py_in_script
 from count_original import count_md_files_in_original
 
-import scripts.generate.count_notes as count_notes
-
+from scripts.generate.count_notes import count_notes
 
 
 if __name__ == "__main__":
