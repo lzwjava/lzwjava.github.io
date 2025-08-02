9fc5cca9c02e60304ed6104db65b14ca2656da37
Zhiwei Li <lzwjava@gmail.com>
2025-08-02 20:26:13 +0800

    feat(scripts): add imagen script for image generation

---
+++ scripts/imagen/imagen.py
@@ -0,0 +1,11 @@
+from google import genai
+from google.genai.types import GenerateImagesConfig
+client = genai.Client()
+image = client.models.generate_images(
+    model="imagen-4.0-generate-preview-06-06",
+    prompt="A dog reading a newspaper",
+    config=GenerateImagesConfig(image_size="2K")
+)
+
+image.generated_images[0].image.save("output-image.png")
+print(f"Created output image using {len(image.generated_images[0].image.image_bytes)} bytes")
