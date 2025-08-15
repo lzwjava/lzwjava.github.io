ad6177909b0e1dd39dea502f550d2b429f7e910e
Zhiwei Li <lzwjava@gmail.com>
2025-08-16 05:56:49 +0800

    fix(mac): use full python3 path in install script

--- scripts/mac/install.py
+++ scripts/mac/install.py
@@ -5,4 +5,4 @@ parser = argparse.ArgumentParser(description='Install Python packages with --bre
 parser.add_argument('package', help='Package name to install')
 args = parser.parse_args()
 
-subprocess.run(['python', '-m', 'pip', 'install', args.package, '--break-system-packages'])
+subprocess.run(['/opt/homebrew/bin/python3', '-m', 'pip', 'install', args.package, '--break-system-packages'])
