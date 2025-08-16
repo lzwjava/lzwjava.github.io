6e2aad80dbb90a2177cdd877b9681fd5fee7ab54
Zhiwei Li <lzwjava@gmail.com>
2025-08-16 14:58:58 +0800

    refactor(count): rename count_py_in_script to count_lang_files

--- scripts/count/count.py
+++ scripts/count/count.py
@@ -3,13 +3,13 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
-from scripts.count.count_lang import count_py_in_script
+from scripts.count.count_lang import count_lang_files
 from count_original import count_md_files_in_original
 
 from scripts.generate.count_notes import count_notes
 
 
 if __name__ == "__main__":
-    count_py_in_script()
+    count_lang_files()
     count_md_files_in_original()
     count_notes()
