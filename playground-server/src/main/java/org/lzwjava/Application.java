aef31b4d5282dd714e9f8e6db7272e78dc52077d
lzwjava <lzwjava@gmail.com>
2025-09-02 17:19:51 +0800

    chore: update build config and suppressions

--- playground-server/src/main/java/org/lzwjava/Application.java
+++ playground-server/src/main/java/org/lzwjava/Application.java
@@ -1,12 +1,8 @@
 package org.lzwjava;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        System.out.println("Application started without Spring Boot");
     }
 }
--- playground-server/src/main/java/org/lzwjava/AspectJConfig.java
+++ playground-server/src/main/java/org/lzwjava/AspectJConfig.java
@@ -1,8 +1,3 @@
 package org.lzwjava;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration
-@EnableAspectJAutoProxy
 public class AspectJConfig {}
