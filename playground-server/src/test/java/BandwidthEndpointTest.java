967ff8cc1ceee42109dfbd5b4ead42891a8e2b60
Zhiwei Li <lzwjava@gmail.com>
2025-09-05 06:06:01 +0800

    test: add basic assertion to BandwidthEndpointTest

--- playground-server/src/test/java/BandwidthEndpointTest.java
+++ playground-server/src/test/java/BandwidthEndpointTest.java
@@ -1,7 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+
 public class BandwidthEndpointTest {
 
     @Test
     void testBandwidthEndpoint() {
+        assertTrue(true);
     }
 }
