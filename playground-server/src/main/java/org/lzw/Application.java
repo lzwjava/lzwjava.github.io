0375849511bbf224c3148024be8dda883c94d8c3
Zhiwei Li <lzwjava@gmail.com>
2025-09-05 05:46:30 +0800

    chore(playground-server): Update checkstyle suppressions and config

--- playground-server/src/main/java/org/lzw/Application.java
+++ playground-server/src/main/java/org/lzw/Application.java
@@ -1,8 +1,13 @@
 package org.lzw;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        System.out.println("Application started without Spring Boot");
+        log.info("Application started without Spring Boot");
     }
 }
